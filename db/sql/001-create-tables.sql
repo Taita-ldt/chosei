364a5623e0ee041142688ed0ad9df9f12769950b
Kazuki Aoshima <kazuki.aoshima@bigtreetc.com>
2020-06-07 01:15:28 +0900

    fix: refs #5 抽選状況テーブルに時刻を保持するカラム追加

--- db/sql/001-create-tables.sql
+++ db/sql/001-create-tables.sql
@@ -32,10 +32,14 @@ create table IF not exists candidate_date_status
  updated_at       timestamp DEFAULT NULL,
     PRIMARY KEY (id)
 );
+
+-- 日比谷公園の抽選状況が格納される。
 create table IF not exists lottery_status
 (
  id               SERIAL,
  candidate_date_id INT NOT NULL,
+ candidate_time_from    timestamp NOT NULL,
+ candidate_time_to    timestamp NOT NULL,
  lottery_status    VARCHAR NOT NULL,
  created_at       timestamp DEFAULT NULL,
  updated_at       timestamp DEFAULT NULL,
