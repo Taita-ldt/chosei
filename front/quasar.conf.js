291f468174d3cceaef7837037cdf0f3dd3e94cc7
會田 剛巳/ aita tsuyoshi <tsuyoshi.aita@bigtreetc.com>
2020-06-07 20:19:40 +0900

    【Front】アプリ名・説明、アイコン変更

--- front/quasar.conf.js
+++ front/quasar.conf.js
@@ -101,9 +101,9 @@ module.exports = function (ctx) {
       workboxPluginMode: 'GenerateSW', // 'GenerateSW' or 'InjectManifest'
       workboxOptions: {}, // only for GenerateSW
       manifest: {
-        name: 'Quasar App',
-        short_name: 'Quasar App',
-        description: 'A Quasar Framework app',
+        name: 'BTCTCポータル',
+        short_name: 'BTCTCポータル',
+        description: 'BTCTCのポータルサイトです。',
         display: 'standalone',
         orientation: 'portrait',
         background_color: '#ffffff',
