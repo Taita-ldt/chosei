9d375a215180c04192d2776834f31f80b2529948
會田 剛巳/ aita tsuyoshi <tsuyoshi.aita@bigtreetc.com>
2020-06-09 21:53:39 +0900

    【API】ヘッダー修正

--- front/src/layouts/main-layout.vue
+++ front/src/layouts/main-layout.vue
@@ -3,10 +3,8 @@
     <q-header elevated>
       <q-toolbar>
         <q-toolbar-title @click="toTop">
-          BTC調整さん
+          BTCテニス部アプリ
         </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
