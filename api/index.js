dadc9d926e60911b4542891c978936942a23f6ff
Taita-ldt <aiko.jitsui@gmail.com>
2022-03-23 02:38:25 +0900

    api:update

--- api/index.js
+++ api/index.js
@@ -26,12 +26,6 @@ app.use(function (req, res, next) {
 // cors対策
 app.use(cors(corsOptions));
 
-models.sequelize.sync().then(() => {
-  logger.info('Start chousei-api');
-}).catch((err) => {
-  logger.error('Something went wrong with the operation: ' + err);
-});
-
 require('./routes/default.route')(app);
 
 // エラーログ出力 
@@ -43,9 +37,12 @@ app.use('/mng', errorHandler.clientErrorHandler);
 // エラーベージ表示用エラーハンドラ
 app.use(errorHandler.errorHandler);
 
-
-const server = http.createServer(app);
-server.listen(port);
-module.exports = app;
+app.listen(port, () => {
+  models.sequelize.sync().then(() => {
+    logger.info('Start chousei-api');
+  }).catch((err) => {
+    logger.error('Something went wrong with the operation: ' + err);
+  });
+});
 
 module.exports.handler = serverless(app); 
